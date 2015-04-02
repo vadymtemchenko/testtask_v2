315ed29ab1e244e243b7b7936e5356925259d33c
tema_kiev <vadym.temch@gmail.com>
2015-04-02 19:27:38 +0300

    revert filters to initial state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,6 +23,10 @@ angular
 				templateUrl: 'views/main.html',
 				controller: 'MainCtrl'
 			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutCtrl'
+			})
 			.otherwise({
 				redirectTo: '/'
 			});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,8 @@ angular.module('addressBook')
 
 			/**
 			 * load data initial data from localStorage or from server
+			 *
+			 * @returns {undefined}
 			 */
 			$scope.init = function() {
 				var self = this,
@@ -15,27 +17,62 @@ angular.module('addressBook')
 						if(rez.data) {
 							localStorage.addressBook = JSON.stringify(rez.data);
 							self.data = rez.data;
+							self.filterRecords();
 						}
 					});
 				} else {
 					$scope.data = JSON.parse(ab);
+					$scope.filterRecords();
 				}
+			};
+
+			/**
+			 * applies filters to data
+			 *
+			 * @returns {undefined}
+			 */
+			$scope.filterRecords = function() {
+				var filters = $scope.filters,
+					filtered;
+
+				filtered = $scope.data.map(function(record, id) {
+					record.id = id;
+					return record;
+				});
+				Object.keys(filters).forEach(function(f) {
+					if(!!filters[f]) {
+						filtered = filtered.filter(function(record) {
+							return record[f].indexOf(filters[f]) > -1;
+						});
+					}
+				});
+				$scope.filtered = filtered;
 			};
 
 			/**
 			 * save changed/add new data to localStorage and send it tot server
+			 *
+			 * @returns {undefined}
 			 */
 			$scope.updateRecord = function(index) {
+				var data;
+
 				if(typeof index === 'undefined') {
 					index = $scope.data.length;
-					$scope.data[index] = $scope.newRecord;
+					data = $scope.newRecord;
 					$scope.newRecord = {};
+				} else {
+					data = $scope.filtered[index];
 				}
-				Backend.updateRecord(index, $scope.data[index]);
+				$scope.data[index] = data;
+				Backend.updateRecord(index, data);
+				$scope.filterRecords();
 			};
 
 			/**
 			 * delete data from localStorage and send it tot server
+			 *
+			 * @returns {undefined}
 			 */
 			$scope.deleteRecord = function(index) {
 				if(typeof index === 'undefined') {
@@ -43,6 +80,7 @@ angular.module('addressBook')
 				}
 				$scope.data.splice(index, 1);
 				Backend.deleteRecord(index);
+				$scope.filterRecords();
 			};
 
 			$scope.filters = {
